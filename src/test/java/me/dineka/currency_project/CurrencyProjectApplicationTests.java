11dfd9588169a44c3dc8b5b5737738c985287c95
Rafnes <rafzograf@gmail.com>
2025-11-07 18:03:36 +0300

    init commit

---
+++ src/test/java/me/dineka/currency_project/CurrencyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dineka.currency_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
